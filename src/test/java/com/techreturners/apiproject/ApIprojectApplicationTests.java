55b3f2e9064dc8320a6b92c536682a8e35705e22
alankm194 <alankm194@gmail.com>
2023-03-16 00:37:29 +0000

    init commit

---
+++ src/test/java/com/techreturners/apiproject/ApIprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techreturners.apiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
